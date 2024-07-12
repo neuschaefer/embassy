db689843555becaea9c5f3573362c8e3d445d484
J. Neuschäfer <j.ne@posteo.net>
2024-07-12 10:13:18 +0200

    serial experiments qemu
    - cd examples/stm32f4
    - objdump -d target/thumbv7em-none-eabi/debug/usart -Mforce-thumb | c++filt | less
    
    - ./cargo b --release &&
      qemu-system-arm -M netduinoplus2 -kernel target/thumbv7em-none-eabi/release/usart -serial stdio -nographic -monitor none
      - it loads and runs correctly
    
    - ./cargo b &&
      qemu-system-arm -M netduinoplus2 -kernel target/thumbv7em-none-eabi/debug/usart -serial stdio -nographic -monitor none
    
    > qemu: fatal: Lockup: can't escalate 3 to HardFault (current priority -1)
    >
    > R00=2002ffe0 R01=00000004 R02=0800be10 R03=00000000
    > R04=00000000 R05=00000000 R06=00000000 R07=00000000
    > R08=00000000 R09=00000000 R10=00000000 R11=00000000
    > R12=00000000 R13=2002ffdc R14=fffffff9 R15=08009100
    > XPSR=61000003 -ZC- T handler
    > s00=00000000 s01=00000000 d00=0000000000000000
    > s02=00000000 s03=00000000 d01=0000000000000000
    > s04=00000000 s05=00000000 d02=0000000000000000
    > s06=00000000 s07=00000000 d03=0000000000000000
    > s08=00000000 s09=00000000 d04=0000000000000000
    > s10=00000000 s11=00000000 d05=0000000000000000
    > s12=00000000 s13=00000000 d06=0000000000000000
    > s14=00000000 s15=00000000 d07=0000000000000000
    > s16=00000000 s17=00000000 d08=0000000000000000
    > s18=00000000 s19=00000000 d09=0000000000000000
    > s20=00000000 s21=00000000 d10=0000000000000000
    > s22=00000000 s23=00000000 d11=0000000000000000
    > s24=00000000 s25=00000000 d12=0000000000000000
    > s26=00000000 s27=00000000 d13=0000000000000000
    > s28=00000000 s29=00000000 d14=0000000000000000
    > s30=00000000 s31=00000000 d15=0000000000000000
    > FPSCR: 00000000
    > Aborted (core dumped)
    
    
    Culprit: the stack
    - with the ELF-loaded program, SP isn't initialzed correctly
      - either don't use elf, or use an early init function to fix this
      - or does QEMU actually do this?
    - memory size: 0000000020000000-000000002001ffff (prio 0, ram): STM32F405.sram
    
    Plan:
    - add a new directory, qemu-stm32f4-netduinoplus2
    - set the correct chip type and memory size
    - use USART1, 0x40011000, PA9/PA10

--- examples/qemu-stm32f4-netduinoplus2/src/bin/usart.rs
+++ examples/qemu-stm32f4-netduinoplus2/src/bin/usart.rs
@@ -2,30 +2,58 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use defmt::*;
-use embassy_stm32::usart::{Config, Uart};
-use embassy_stm32::{bind_interrupts, peripherals, usart};
-use {defmt_rtt as _, panic_probe as _};
+use embassy_stm32::{Config, bind_interrupts, peripherals, usart};
+use embassy_stm32::usart::Uart;
+use embassy_stm32::rcc::Sysclk;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+        loop {}
+}
 
 bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
 
+fn putc(c: char) {
+    use core::sync::atomic::Ordering;
+    use core::sync::atomic::compiler_fence;
+    unsafe {
+        let usart = 0x40011000 as *mut u32;
+        *usart.add(1) = c as u32;
+        compiler_fence(Ordering::SeqCst);
+    }
+}
+
 #[entry]
 fn main() -> ! {
-    info!("Hello World!");
+    putc('0');
+    //info!("Hello World!");
+    putc('1');
 
-    let p = embassy_stm32::init(Default::default());
+    // QEMU doesn't emulate the reset/clock controller (RCC), so don't try to
+    // initialize it.
+    let mut config: Config = Default::default();
+    config.rcc.pll = None;
+    config.rcc.hse = None;
+    config.rcc.sys = Sysclk::HSI;
+    let p = embassy_stm32::init(config);
 
-    let config = Config::default();
+    putc('2');
+
+    let config = usart::Config::default();
     let mut usart = Uart::new_blocking(p.USART1, p.PA10, p.PA9, config).unwrap();
+    putc('3');
 
-    unwrap!(usart.blocking_write(b"Hello Embassy World!\r\n"));
-    info!("wrote Hello, starting echo");
+    /*unwrap!(*/usart.blocking_write(b"Hello Embassy World!\r\n")/*)*/;
+    //info!("wrote Hello, starting echo");
+    putc('4');
 
     let mut buf = [0u8; 1];
     loop {
-        unwrap!(usart.blocking_read(&mut buf));
-        unwrap!(usart.blocking_write(&buf));
+        usart.blocking_read(&mut buf);
+        usart.blocking_write(&buf);
     }
 }
--- examples/stm32f4/src/bin/usart.rs
+++ examples/stm32f4/src/bin/usart.rs
@@ -8,24 +8,34 @@ use embassy_stm32::{bind_interrupts, peripherals, usart};
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    USART3 => usart::InterruptHandler<peripherals::USART3>;
+    USART1 => usart::InterruptHandler<peripherals::USART1>;
 });
+
+fn putc(c: char) {
+    use core::sync::atomic::Ordering;
+    use core::sync::atomic::compiler_fence;
+    unsafe {
+        let usart = 0x40011000 as *mut u32;
+        *usart.add(1) = c as u32;
+        compiler_fence(Ordering::SeqCst);
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    info!("Hello World!");
-
+    putc('0');
     let p = embassy_stm32::init(Default::default());
+    putc('1');
 
     let config = Config::default();
-    let mut usart = Uart::new_blocking(p.USART3, p.PD9, p.PD8, config).unwrap();
+    let mut usart = Uart::new_blocking(p.USART1, p.PD9, p.PD8, config).unwrap();
+    putc('2');
 
     unwrap!(usart.blocking_write(b"Hello Embassy World!\r\n"));
-    info!("wrote Hello, starting echo");
+    putc('3');
 
-    let mut buf = [0u8; 1];
+
     loop {
-        unwrap!(usart.blocking_read(&mut buf));
-        unwrap!(usart.blocking_write(&buf));
+        putc('A');
     }
 }
